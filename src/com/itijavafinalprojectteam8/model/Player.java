b9ebc0f2aceb1598b9e3ecd44ab61f9222489f50
EsraaSalah28 <es83626@gmail.com>
2020-01-03 15:11:07 +0200

    somecode

--- src/com/itijavafinalprojectteam8/model/Player.java
+++ src/com/itijavafinalprojectteam8/model/Player.java
@@ -1,4 +1,32 @@
 package com.itijavafinalprojectteam8.model;
 
 public class Player {
+      String player_name;
+        String player_email;
+        String player_password;
+
+    public String getPlayer_name() {
+        return player_name;
+    }
+
+    public void setPlayer_name(String player_name) {
+        this.player_name = player_name;
+    }
+
+    public String getPlayer_email() {
+        return player_email;
+    }
+
+    public void setPlayer_email(String player_email) {
+        this.player_email = player_email;
+    }
+
+    public String getPlayer_password() {
+        return player_password;
+    }
+
+    public void setPlayer_password(String player_password) {
+        this.player_password = player_password;
+    }
+        
 }
